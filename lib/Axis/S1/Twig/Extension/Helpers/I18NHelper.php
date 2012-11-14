c04f0d01306dba52a722dafbc09efe5cc3e7d3d4
7heaven <7heaven@75cfc154-6186-4322-82f4-1edd5a5e6421>
2012-11-14 01:09:40 +0000

    Fixed bug

--- lib/Axis/S1/Twig/Extension/Helpers/I18NHelper.php
+++ lib/Axis/S1/Twig/Extension/Helpers/I18NHelper.php
@@ -24,10 +24,10 @@ class I18NHelper extends \Twig_Extension
     return array(
       'i18n'                 => new \Twig_Filter_Function('__'),
       't'                    => new \Twig_Filter_Function('__'),
-      '__'                   => new \Twig_Function_Function('__'),
-      'format_number_choice' => new \Twig_Function_Function('format_number_choice'),
-      'format_country'       => new \Twig_Function_Function('format_country'),
-      'format_language'      => new \Twig_Function_Function('format_language'),
+      '__'                   => new \Twig_Filter_Function('__'),
+      'format_number_choice' => new \Twig_Filter_Function('format_number_choice'),
+      'format_country'       => new \Twig_Filter_Function('format_country'),
+      'format_language'      => new \Twig_Filter_Function('format_language'),
     );
   }
 
