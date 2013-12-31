4d9affb4e17d3ecd35c2a22f91b0108bd0fa0b13
alex <alex@ansata.biz>
2013-12-31 18:35:22 +0200

    {{ include_component }} would do "echo include_component()" what results in "1" after the output

--- lib/Axis/S1/Twig/Extension/Helpers/PartialHelper.php
+++ lib/Axis/S1/Twig/Extension/Helpers/PartialHelper.php
@@ -13,18 +13,38 @@ class PartialHelper extends \Twig_Extension
   {
     return array(
       'include_component_slot' => new \Twig_Function_Function('include_component_slot'),
-      'get_component_slot'     => new \Twig_Function_Function('get_component_slot'),
-      'has_component_slot'     => new \Twig_Function_Function('has_component_slot'),
-      'include_component'      => new \Twig_Function_Function('include_component'),
-      'get_component'          => new \Twig_Function_Function('get_component'),
-      'include_partial'        => new \Twig_Function_Function('include_partial'),
-      'get_partial'            => new \Twig_Function_Function('get_partial'),
-      'slot'                   => new \Twig_Function_Function('slot'),
-      'end_slot'               => new \Twig_Function_Function('end_slot'),
-      'has_slot'               => new \Twig_Function_Function('has_slot'),
-      'include_slot'           => new \Twig_Function_Function('include_slot'),
-      'get_slot'               => new \Twig_Function_Function('get_slot'),
+      'get_component_slot' => new \Twig_Function_Function('get_component_slot'),
+      'has_component_slot' => new \Twig_Function_Function('has_component_slot'),
+
+      'include_component' => new \Twig_SimpleFunction('include_component', array(
+          $this,
+          'include_component'
+        ), array('is_safe' => array('html'))),
+
+      'get_component' => new \Twig_Function_Function('get_component'),
+      'include_partial' => new \Twig_Function_Function('include_partial'),
+      'get_partial' => new \Twig_Function_Function('get_partial'),
+      'slot' => new \Twig_Function_Function('slot'),
+      'end_slot' => new \Twig_Function_Function('end_slot'),
+      'has_slot' => new \Twig_Function_Function('has_slot'),
+
+      'include_slot' => new \Twig_SimpleFunction('include_slot', array(
+          $this,
+          'include_slot'
+        ), array('is_safe' => array('html'))),
+
+      'get_slot' => new \Twig_Function_Function('has_slot'),
     );
+  }
+
+  public function include_slot($name, $default = '')
+  {
+    return get_slot($name, $default);
+  }
+
+  public function include_component($moduleName, $componentName, $vars = array())
+  {
+    return get_component($moduleName, $componentName, $vars);
   }
 
   public function getName()
